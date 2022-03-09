b335e50215e9b0e485512bdcb1031c89f8abe3b2
fjost <jost@mobile-software.ag>
2022-03-09 17:29:43 +0100

    feat: add user entity

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
