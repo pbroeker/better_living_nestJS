bb7e9c0a2a755cefb4c869c6afa40ec83d118b48
pbroeker <broeker@mobile-software.ag>
2022-03-21 13:51:14 +0100

    Removing dto for query-param

--- src/core/translation/dto/translation.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-
-export class TranslationDto {
-  @ApiProperty()
-  @IsString()
-  language: 'en' | 'de';
-}
