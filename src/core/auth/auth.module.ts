4405062c5dbc88015fac8fcbf15a2db2b3919222
fjost <jost@mobile-software.ag>
2022-03-21 14:49:55 +0100

    feat: init module

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { SharedModule } from 'src/shared/shared.module';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
-import { SharedModule } from 'src/shared/shared.module';
 @Module({
-  imports: [UsersModule, SharedModule],
+  imports: [
+    UsersModule,
+    SharedModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET'),
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
