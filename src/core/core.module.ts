a6917fb32af2d07d463ffa2308af3b78e6b89ec3
pbroeker <broeker@mobile-software.ag>
2022-03-21 12:50:52 +0100

    ADD: TranslationModule base

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { TranslationModule } from './translation/translation.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UsersModule],
+  imports: [AuthModule, DatabaseModule, UsersModule, TranslationModule],
 })
 export class CoreModule {}
