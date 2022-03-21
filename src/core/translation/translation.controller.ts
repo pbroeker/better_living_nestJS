4d75eba39de01661ccde4445d775fbbe8efed5fe
pbroeker <broeker@mobile-software.ag>
2022-03-21 13:51:33 +0100

    Using queryParam in translation controller

--- src/core/translation/translation.controller.ts
+++ src/core/translation/translation.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { TranslationDto } from './dto/translation.dto';
+import { Controller, Get, Query } from '@nestjs/common';
 import { TranslationService } from './translation.service';
 
 @Controller('translation')
 export class TranslationController {
   constructor(private translationService: TranslationService) {}
 
-  @Get('/:language')
-  async getLanguageJson(@Param('language') language: TranslationDto) {
-    return this.translationService.getLanguageJson(language);
+  @Get()
+  async getLanguageJson(@Query('locale') locale: string) {
+    return await this.translationService.getLanguageJson(locale);
   }
 }
