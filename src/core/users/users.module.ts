6dfedf8c76f3443d3d15740d230e12aafc0c7c56
pbroeker <broeker@mobile-software.ag>
2022-04-11 14:52:14 +0200

    FIX: No areaCreation in userService

--- src/core/users/users.module.ts
+++ src/core/users/users.module.ts
@@ -3,16 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './users.service';
 import { CoreUser } from './entity/user.entity';
 import { SharedModule } from '../../shared/shared.module';
-import { PersonalAreaService } from 'src/feature/personal-areas/personal-area.service';
-import { PersonalRoom } from 'src/feature/personal-room/entity/personalRoom.entity';
-import { PersonalArea } from 'src/feature/personal-areas/entity/personalArea.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([CoreUser, PersonalRoom, PersonalArea]),
-    SharedModule,
-  ],
-  providers: [UserService, PersonalAreaService],
+  imports: [TypeOrmModule.forFeature([CoreUser]), SharedModule],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UsersModule {}
--- src/core/users/users.service.ts
+++ src/core/users/users.service.ts
@@ -3,13 +3,11 @@ import { Repository } from 'typeorm';
 import { CoreUser } from './entity/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SharedAuthService } from '../../shared/shared-auth.service';
-import { PersonalAreaService } from 'src/feature/personal-areas/personal-area.service';
 import { CoreUserDto } from './dto/core-user.dto';
 @Injectable()
 export class UserService {
   constructor(
     private sharedAuthServiceService: SharedAuthService,
-    private personalAreaService: PersonalAreaService,
     @InjectRepository(CoreUser)
     private userRepository: Repository<CoreUser>,
   ) {}
@@ -28,16 +26,6 @@ export class UserService {
         user_password: pwHash,
       });
       const savedUserEntity = await this.userRepository.save(userEntity);
-      await this.personalAreaService.createPersonalArea(
-        {
-          title: 'Unassigned',
-          personalRoomIds: [],
-        },
-        {
-          userId: savedUserEntity.id,
-          email: savedUserEntity.user_email,
-        },
-      );
       return { userId: savedUserEntity.id, email: savedUserEntity.user_email };
     } catch (error) {
       throw new HttpException(
