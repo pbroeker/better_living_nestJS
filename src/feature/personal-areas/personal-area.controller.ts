77d00486eb7767752772d7ee300bef0bbc067aaa
pbroeker <broeker@mobile-software.ag>
2022-04-20 10:26:42 +0200

    ADD: controller & service to edit personalAreas

--- src/feature/personal-areas/personal-area.controller.ts
+++ src/feature/personal-areas/personal-area.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, HttpStatus, Post } from '@nestjs/common';
-import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiResponse } from '@nestjs/swagger';
 import { CoreUserDto } from 'src/core/users/dto/core-user.dto';
 import { User } from 'src/utils/customDecorators/user.decorator';
 import {
@@ -39,11 +48,35 @@ export class PersonalAreaController {
     description: 'Personal area could not be created',
   })
   @Post()
-  async savePersonalRooms(
+  async createPersonalArea(
     @Body() personalAreaReqDto: PersonalAreaReqDto,
     @User() user: CoreUserDto,
   ): Promise<PersonalAreaResDto> {
     return await this.personalAreaService.createPersonalArea(
+      personalAreaReqDto,
+      user,
+    );
+  }
+
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Personal area edited',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Personal area could not be edited',
+  })
+  @ApiBody({
+    type: PersonalAreaReqDto,
+  })
+  @Put('/:areaId')
+  async editPersonalArea(
+    @Param('areaId', ParseIntPipe) areaId: number,
+    @Body() personalAreaReqDto: PersonalAreaReqDto,
+    @User() user: CoreUserDto,
+  ): Promise<PersonalAreaResDto> {
+    return await this.personalAreaService.editPersonalArea(
+      areaId,
       personalAreaReqDto,
       user,
     );
