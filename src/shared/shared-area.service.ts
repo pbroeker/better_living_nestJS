36eab8bcb0db92b980f32caff1407e1c7af3a115
pbroeker <broeker@mobile-software.ag>
2022-04-21 16:11:56 +0200

    ADD: SharedAreaService

---
+++ src/shared/shared-area.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CoreUser } from '../core/users/entity/user.entity';
+import { PersonalArea } from '../feature/personal-areas/entity/personalArea.entity';
+
+@Injectable()
+export class SharedAreaService {
+  constructor(
+    @InjectRepository(PersonalArea)
+    private personalAreaRepository: Repository<PersonalArea>,
+  ) {}
+
+  async createNewArea(
+    currentUser: CoreUser,
+    title = 'Unassigned',
+  ): Promise<PersonalArea> {
+    const createdNewArea = this.personalAreaRepository.create({
+      user: currentUser,
+      title: title,
+    });
+
+    const savedPersonalArea = await this.personalAreaRepository.save(
+      createdNewArea,
+    );
+    return savedPersonalArea;
+  }
+
+  async findAll(currentUser: CoreUser): Promise<PersonalArea[]> {
+    return this.personalAreaRepository.find({ where: { user: currentUser } });
+  }
+
+  async findByTitle(
+    currentUser: CoreUser,
+    title: string,
+  ): Promise<PersonalArea> {
+    const foundArea = await this.personalAreaRepository.findOne({
+      where: { user: currentUser, title: title },
+    });
+    return foundArea;
+  }
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonalArea } from '../feature/personal-areas/entity/personalArea.entity';
 import { CoreUser } from '../core/users/entity/user.entity';
 import { SharedAuthService } from './shared-auth.service';
 import { SharedUserService } from './shared-user.service';
+import { SharedAreaService } from './shared-area.service';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([CoreUser])],
-  providers: [SharedAuthService, SharedUserService],
-  exports: [SharedAuthService, SharedUserService],
+  imports: [ConfigModule, TypeOrmModule.forFeature([CoreUser, PersonalArea])],
+  providers: [SharedAuthService, SharedUserService, SharedAreaService],
+  exports: [SharedAuthService, SharedUserService, SharedAreaService],
 })
 export class SharedModule {}
