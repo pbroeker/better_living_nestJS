bcfbc1175738c40246e56306a609c2195628f1f1
pbroeker <broeker@mobile-software.ag>
2022-04-21 16:15:04 +0200

    REFACTOR: Cleanup and small fixes

--- src/feature/personal-room/personal-room.module.ts
+++ src/feature/personal-room/personal-room.module.ts
@@ -4,13 +4,9 @@ import { PersonalRoomController } from './personal-room.controller';
 import { PersonalRoomService } from './personal-room.service';
 import { PersonalRoom } from './entity/personalRoom.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PersonalArea } from '../personal-areas/entity/personalArea.entity';
 
 @Module({
-  imports: [
-    SharedModule,
-    TypeOrmModule.forFeature([PersonalRoom, PersonalArea]),
-  ],
+  imports: [SharedModule, TypeOrmModule.forFeature([PersonalRoom])],
 
   controllers: [PersonalRoomController],
   providers: [PersonalRoomService],
