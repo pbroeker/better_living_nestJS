0565f387383c7fa313d7001e7f0901283a44ea12
pbroeker <broeker@mobile-software.ag>
2022-04-22 11:29:05 +0200

    REFACTOR: Cleanup

--- src/feature/personal-areas/dto/personal-area.dto.ts
+++ src/feature/personal-areas/dto/personal-area.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsArray,
+  IsDateString,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { PersonalRoomResDto } from 'src/feature/personal-room/dto/personal-room.dto';
 
 export class PersonalAreaReqDto {
