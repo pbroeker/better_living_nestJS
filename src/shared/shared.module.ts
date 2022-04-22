be291a399cfe345820dd36319f316aa57093041e
pbroeker <broeker@mobile-software.ag>
2022-04-22 09:26:22 +0200

    FIX: Missing typeORMEntities in sharedModule

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -6,9 +6,14 @@ import { SharedAuthService } from './shared-auth.service';
 import { SharedUserService } from './shared-user.service';
 import { SharedAreaService } from './shared-area.service';
 import { SharedRoomService } from './shared-room.service';
+import { PersonalArea } from 'src/feature/personal-areas/entity/personalArea.entity';
+import { PersonalRoom } from 'src/feature/personal-room/entity/personalRoom.entity';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([CoreUser])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([CoreUser, PersonalArea, PersonalRoom]),
+  ],
   providers: [
     SharedAuthService,
     SharedUserService,
