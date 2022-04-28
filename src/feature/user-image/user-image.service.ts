debcf9634b70e8951051aea7224f7ca4fc803bd3
fjost <jost@mobile-software.ag>
2022-04-28 11:28:32 +0200

    feat: increase field size limit

--- src/feature/user-image/user-image.service.ts
+++ src/feature/user-image/user-image.service.ts
@@ -21,6 +21,7 @@ export class UserImageService {
   private readonly AWS_S3_BUCKET_NAME = this.configService.get('BUCKET');
   private readonly s3 = new AWS.S3();
   private readonly upload = multer({
+    limits: { fieldSize: 25 * 1024 * 1024 },
     storage: multerS3({
       s3: this.s3,
       bucket: this.AWS_S3_BUCKET_NAME,
