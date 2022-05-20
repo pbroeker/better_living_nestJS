af61c118702eb7f409db1e5e2dee4cb02f37972f
fjost <jost@mobile-software.ag>
2022-05-20 13:09:27 +0200

    feat: create token service

--- src/feature/invitation-token/invitation-token.service.ts
+++ src/feature/invitation-token/invitation-token.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CoreUserDto } from '../../core/users/dto/core-user.dto';
 import { Repository } from 'typeorm';
+import { CoreUserDto } from '../../core/users/dto/core-user.dto';
+import { SharedUserService } from '../../shared/shared-user.service';
+import { InvitationTokenResDto } from './dto/invitation-token.dto';
 import { InvitationToken } from './entity/invitation-token.entity';
 
 @Injectable()
@@ -9,9 +11,32 @@ export class InvitationTokenService {
   constructor(
     @InjectRepository(InvitationToken)
     private invitationTokenRepo: Repository<InvitationToken>,
+    private sharedUserService: SharedUserService,
   ) {}
 
   async createInvitationToken(inviter: CoreUserDto) {
-    return null;
+    try {
+      const activeCoreUser = await this.sharedUserService.findByEmail(
+        inviter.email,
+      );
+      const newInvitationTokenEntity = this.invitationTokenRepo.create({
+        inviter: activeCoreUser,
+      });
+      const savedInvitationTokenEntity = await this.invitationTokenRepo.save(
+        newInvitationTokenEntity,
+      );
+      return {
+        invitationToken: savedInvitationTokenEntity.token,
+        inviter: activeCoreUser.user_email,
+      } as InvitationTokenResDto;
+    } catch (error) {
+      throw new HttpException(
+        {
+          title: 'invitation_token.error.create_token.title',
+          text: 'invitation_token.error.create_token.message',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
