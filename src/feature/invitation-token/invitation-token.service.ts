7552af18e8d961c577951b7bad8d8e1e0664d47d
pbroeker <broeker@mobile-software.ag>
2022-05-31 14:00:57 +0200

    ADD: Controller & Service for checking invitationToken

--- src/feature/invitation-token/invitation-token.service.ts
+++ src/feature/invitation-token/invitation-token.service.ts
@@ -1,5 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CoreUser } from 'src/core/users/entity/user.entity';
 import { Repository } from 'typeorm';
 import { CoreUserDto } from '../../core/users/dto/core-user.dto';
 import { SharedUserService } from '../../shared/shared-user.service';
@@ -36,6 +37,50 @@ export class InvitationTokenService {
           text: 'invitation_token.error.create_token.message',
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async checkInvitationToken(
+    invitedUser: CoreUserDto,
+    invitationToken: string,
+  ): Promise<CoreUser> {
+    try {
+      const activeCoreUser = await this.sharedUserService.findByEmail(
+        invitedUser.email,
+      );
+
+      const foundInvitationToken = await this.invitationTokenRepo.findOne({
+        where: { token: invitationToken },
+        relations: ['inviter'],
+      });
+
+      if (foundInvitationToken) {
+        await this.sharedUserService.addGuest(
+          foundInvitationToken.inviter,
+          activeCoreUser,
+        );
+        return foundInvitationToken.inviter;
+      } else {
+        throw new HttpException(
+          {
+            title: 'invitation_token.error.expired.title',
+            text: 'invitation_token.error.expired.message',
+          },
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    } catch (error) {
+      throw new HttpException(
+        {
+          title: error.response?.title
+            ? error.response.title
+            : 'invitation_token.error.check_token.title',
+          text: error.response?.text
+            ? error.response.text
+            : 'invitation_token.error.check_token.title',
+        },
+        error.status ? error.status : HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
