3160973022d53ce5db43393fe8f55bfb9db72d31
pbroeker <broeker@mobile-software.ag>
2022-06-08 12:11:38 +0200

    ADD: Helper for initals

--- src/utils/features/helpers.ts
+++ src/utils/features/helpers.ts
@@ -28,3 +28,19 @@ export const removeDateStrings = <T extends HasDates>(
   const { createdAt, updatedAt, ...entityWithoutDates } = entityWithDates;
   return entityWithoutDates;
 };
+
+export const getUserInitials = (user: CoreUser): string => {
+  if (user.first_name && user.last_name) {
+    return `${user.first_name.charAt(0).toUpperCase()}.${user.last_name
+      .charAt(0)
+      .toUpperCase()}.`;
+  } else if (user.first_name) {
+    return `${user.first_name.charAt(0).toUpperCase()}.${user.first_name
+      .charAt(1)
+      .toUpperCase()}.`;
+  } else {
+    return `${user.user_email.charAt(0).toUpperCase()}.${user.user_email
+      .charAt(1)
+      .toUpperCase()}.`;
+  }
+};
