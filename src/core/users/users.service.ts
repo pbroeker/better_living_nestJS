796117e2c4947433fecb9d7720cd1efa959c6558
pbroeker <broeker@mobile-software.ag>
2022-06-21 08:49:38 +0200

    ADD: Check for decoded PW being min 4 char long

--- src/core/users/users.service.ts
+++ src/core/users/users.service.ts
@@ -13,30 +13,30 @@ export class UserService {
   ) {}
 
   async createUser(registerUserDto: RegisterUserReqDto): Promise<CoreUser> {
-    try {
-      const decodedPassword = Buffer.from(
-        registerUserDto.password,
-        'base64',
-      ).toString();
-      const pwHash = await this.sharedAuthServiceService.hashPassword(
-        decodedPassword,
-      );
-      const userEntity = this.userRepository.create({
-        ...registerUserDto,
-        user_email: registerUserDto.email,
-        user_password: pwHash,
-      });
+    const decodedPassword = Buffer.from(
+      registerUserDto.password,
+      'base64',
+    ).toString();
 
-      const savedUserEntity = await this.userRepository.save(userEntity);
-      return savedUserEntity;
-    } catch (error) {
+    if (decodedPassword.length < 4) {
       throw new HttpException(
         {
-          title: 'login.error.create_user.title',
-          text: 'login.error.create_user.message',
+          title: 'login.error.short_pw.title',
+          text: 'login.error.short_pw.message',
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
+    const pwHash = await this.sharedAuthServiceService.hashPassword(
+      decodedPassword,
+    );
+    const userEntity = this.userRepository.create({
+      ...registerUserDto,
+      user_email: registerUserDto.email,
+      user_password: pwHash,
+    });
+
+    const savedUserEntity = await this.userRepository.save(userEntity);
+    return savedUserEntity;
   }
 }
