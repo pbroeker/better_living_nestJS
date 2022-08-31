cbb0f37c984e337cc8ec77bf5c83af41ef807bb4
pbroeker <broeker@mobile-software.ag>
2022-08-31 09:45:00 +0200

    REFACTOR: PersonalAreaDto & Service

--- src/feature/personal-areas/dto/personal-area.dto.ts
+++ src/feature/personal-areas/dto/personal-area.dto.ts
@@ -1,12 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
+  Exclude,
+  Expose,
+  instanceToPlain,
+  plainToInstance,
+  Transform,
+} from 'class-transformer';
+import {
   IsArray,
   IsBoolean,
-  IsNumber,
   IsOptional,
   IsString,
+  IsUUID,
 } from 'class-validator';
+import { getUserInitials } from 'src/utils/features/helpers';
 import { PersonalRoomResDto } from '../../../feature/personal-room/dto/personal-room.dto';
+import { PersonalArea } from '../entity/personalArea.entity';
 
 export class PersonalAreaReqDto {
   @ApiProperty()
@@ -18,14 +27,17 @@ export class PersonalAreaReqDto {
   personalRoomIds: number[];
 }
 
+Exclude();
 export class PersonalAreaResDto {
   @ApiProperty()
+  @Expose()
   @IsString()
   title: string;
 
   @ApiProperty()
+  @Expose()
   @IsOptional()
-  @IsNumber()
+  @IsUUID()
   id: number;
 
   @ApiProperty()
@@ -34,12 +46,28 @@ export class PersonalAreaResDto {
   isOwner?: boolean;
 
   @ApiProperty()
+  @Expose()
   @IsOptional()
   @IsString()
+  @Transform(({ obj }: { obj: PersonalArea }) => {
+    return getUserInitials(obj.owner);
+  })
   ownerInitials?: string;
 
   @ApiProperty({ type: [PersonalRoomResDto] })
+  @Expose()
   @IsOptional()
   @IsArray()
+  @Transform(({ obj }: { obj: PersonalArea }) => {
+    return obj.personalRooms.map((personalRoomEntity) => {
+      return plainToInstance(
+        PersonalRoomResDto,
+        instanceToPlain(personalRoomEntity),
+        {
+          excludeExtraneousValues: true,
+        },
+      );
+    });
+  })
   personalRooms?: PersonalRoomResDto[];
 }
